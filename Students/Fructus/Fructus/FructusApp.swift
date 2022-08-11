63150eca60323719aa15fa57d7237348345a3f63
Rafael Fernandez <myztajay@hotmail.com>
2022-08-11 15:05:57 -0700

    Added color pallets and other resources

---
+++ Students/Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Rafael Fernandez on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
